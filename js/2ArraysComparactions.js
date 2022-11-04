61004ca3963c5256551c66b91f6f0866a4aa48d7
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-03 22:11:45 -0300

    feat: teste 2 arrays comparactions

---
+++ js/2ArraysComparactions.js
@@ -0,0 +1,28 @@
+const productItens = [
+              {
+                "productId": "370980-3",
+                "quantity": 1,
+                "itemPrice": 1499.9,
+                "currency": "BRL"
+            },
+            {
+                "productId": "310980-3",
+                "quantity": 2,
+                "itemPrice": 1500.1,
+                "currency": "BRL"
+            }
+          ]
+const productNames = [
+			{
+				"productId": "370980-3",
+				"nomeProduto": "Pneu Ecology Xbri 185/60 R14 82H - Preto"
+			},
+			{
+				"productId": "310985-3",
+				"nomeProduto": "Pneu Ecology Xbri 175/75 R14 86T - Preto"
+			},
+			{
+				"productId": "310980-3",
+				"nomeProduto": "Pneu Ecology Xbri 175/75 R14 86T - Preto"
+			}
+]
