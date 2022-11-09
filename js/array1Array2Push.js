0c0db36149f3d75e9ed7b9c212617b216b9db525
joaopfsiqueira <joaopfsiqueiraa@gmail.com>
2022-11-09 08:27:48 -0300

    feat: adicionando todos objetos de array em outro

---
+++ js/array1Array2Push.js
@@ -0,0 +1,137 @@
+let array = [];
+
+
+let productItems = [
+    {
+        "productId": "378446-1",
+        "nomeProduto": "Smart Tv Philco Led 55\" Roku 4K Dolby Ptv55g52r2c - Bivolt"
+    },
+    {
+        "productId": "377056-1",
+        "nomeProduto": "Smart Tv Philips Led 65\" Hdr C/ Android E Google Assistant - Bivolt"
+    },
+    {
+        "productId": "369841-1",
+        "nomeProduto": "Notebook Asus Amd Ryzen 5 8Gb Ram 256Gb Ssd Tela 15,6\" W11 - Cinza"
+    },
+    {
+        "productId": "377504-1",
+        "nomeProduto": "Fone De Ouvido Waaw By Alok Energy 100Ebg Modo Gamer - Preto"
+    },
+    {
+        "productId": "374215-1",
+        "nomeProduto": "Notebook Acer Aspire 3 Intel Celeron 15,6\" 4G+128Gb W11 - Bivolt"
+    },
+    {
+        "productId": "368349-1",
+        "nomeProduto": "Central Multim¡dia Pionner 6,2\" Bluetooth Touch Screen Dmha248bt - Preto"
+    },
+    {
+        "productId": "366701-1",
+        "nomeProduto": "Smart Tv 50\" Philips Led Uhd 4K C/ Android"
+    },
+    {
+        "productId": "372261-1",
+        "nomeProduto": "Smart Tv Philco 32? Hd Roku Tv C/ Dolby µudio"
+    },
+    {
+        "productId": "375058-1",
+        "nomeProduto": "Smart Box 4K Android Aqu rio - Bivolt"
+    },
+    {
+        "productId": "376197-1",
+        "nomeProduto": "Smart Tv Led 43\" Fhd Android S615 Tcl - Bivolt"
+    },
+    {
+        "productId": "381049-1",
+        "nomeProduto": "Notebook Lenovo 15,6\" Intel Core I3 4G+256Gb W11 - Bivolt"
+    },
+    {
+        "productId": "343626-2",
+        "nomeProduto": "Caixa de Som Bluetooth JBL Boombox 2 - Preto"
+    },
+    {
+        "productId": "365674-2",
+        "nomeProduto": "Tablet Kids Philco Ptb7ssg Android 9 Pie Multi-Toque 7? - Azul"
+    },
+    {
+        "productId": "377503-1",
+        "nomeProduto": "Fone De Ouvido Waaw By Alok Sense 100Hw Headset - Preto"
+    },
+    {
+        "productId": "365128-1",
+        "nomeProduto": "Notebook Asus Intel Celeron 4Gb Ram 128Gb Ssd Tela 15,6\" W11 Pro - Bivolt"
+    }
+]
+
+let productItems2 = [
+    {
+        "productId": "378446-1",
+        "nomeProduto": "Smart Tv Philco Led 55\" Roku 4K Dolby Ptv55g52r2c - Bivolt"
+    },
+    {
+        "productId": "377056-1",
+        "nomeProduto": "Smart Tv Philips Led 65\" Hdr C/ Android E Google Assistant - Bivolt"
+    },
+    {
+        "productId": "369841-1",
+        "nomeProduto": "Notebook Asus Amd Ryzen 5 8Gb Ram 256Gb Ssd Tela 15,6\" W11 - Cinza"
+    },
+    {
+        "productId": "377504-1",
+        "nomeProduto": "Fone De Ouvido Waaw By Alok Energy 100Ebg Modo Gamer - Preto"
+    },
+    {
+        "productId": "374215-1",
+        "nomeProduto": "Notebook Acer Aspire 3 Intel Celeron 15,6\" 4G+128Gb W11 - Bivolt"
+    },
+    {
+        "productId": "368349-1",
+        "nomeProduto": "Central Multim¡dia Pionner 6,2\" Bluetooth Touch Screen Dmha248bt - Preto"
+    },
+    {
+        "productId": "366701-1",
+        "nomeProduto": "Smart Tv 50\" Philips Led Uhd 4K C/ Android"
+    },
+    {
+        "productId": "372261-1",
+        "nomeProduto": "Smart Tv Philco 32? Hd Roku Tv C/ Dolby µudio"
+    },
+    {
+        "productId": "375058-1",
+        "nomeProduto": "Smart Box 4K Android Aqu rio - Bivolt"
+    },
+    {
+        "productId": "376197-1",
+        "nomeProduto": "Smart Tv Led 43\" Fhd Android S615 Tcl - Bivolt"
+    },
+    {
+        "productId": "381049-1",
+        "nomeProduto": "Notebook Lenovo 15,6\" Intel Core I3 4G+256Gb W11 - Bivolt"
+    },
+    {
+        "productId": "343626-2",
+        "nomeProduto": "Caixa de Som Bluetooth JBL Boombox 2 - Preto"
+    },
+    {
+        "productId": "365674-2",
+        "nomeProduto": "Tablet Kids Philco Ptb7ssg Android 9 Pie Multi-Toque 7? - Azul"
+    },
+    {
+        "productId": "377503-1",
+        "nomeProduto": "Fone De Ouvido Waaw By Alok Sense 100Hw Headset - Preto"
+    },
+    {
+        "productId": "365128-1",
+        "nomeProduto": "Notebook Asus Intel Celeron 4Gb Ram 128Gb Ssd Tela 15,6\" W11 Pro - Bivolt"
+    }
+]
+
+
+
+array.push(...productItems)
+array.push(...productItems2)
+console.log(array)
+
+
+
